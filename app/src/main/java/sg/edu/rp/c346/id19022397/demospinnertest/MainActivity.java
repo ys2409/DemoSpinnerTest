e3ac3956bfcc71ebfe0e1720b4c68494e4a47512
ys2409 <19022397@myrp.edu.sg>
2020-06-09 14:10:26 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19022397/demospinnertest/MainActivity.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id19022397.demospinnertest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Spinner spnYesNo;
+    TextView tvDisplay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spnYesNo = findViewById(R.id.spinner);
+        tvDisplay = findViewById(R.id.textView);
+
+        spnYesNo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position){
+                    case 0:
+                        tvDisplay.setText("Spinner Item, Yes Selected");
+                        break;
+                    case 1:
+                        tvDisplay.setText("Spinner Item, No Selected");
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+
+}
+
